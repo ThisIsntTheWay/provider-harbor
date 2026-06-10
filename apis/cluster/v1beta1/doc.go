f1bb2041aa3eb79e19069dc8cfe82f8efc80f5ac
ThisIsntTheWay <ThisIsntTheWay@users.noreply.github.com>
2026-06-10 18:30:33 +0200

    Fix malformed API group

--- apis/cluster/v1beta1/doc.go
+++ apis/cluster/v1beta1/doc.go
@@ -1,5 +1,5 @@
 // Package v1beta1 contains the core resources of the harbor upjet provider.
 // +kubebuilder:object:generate=true
-// +groupName=harbor.
+// +groupName=harbor.crossplane.io
 // +versionName=v1beta1
 package v1beta1
